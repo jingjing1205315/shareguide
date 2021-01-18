9de9654c4f959fdcdb8407e3a20fb31fcd82be57
April <zhangyajing1205315@163.com>
2021-01-18 20:20:01 +0800

    1

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 import app from './src/index.vue';
+console.log('npm', app)
 export default app;
--- src/index.vue
+++ src/index.vue
@@ -10,7 +10,7 @@ export default {
     data: function () {
         return {};
     },
-    props: {},
+    props: ['text'],
     methods: {},
     components: {},
 };
